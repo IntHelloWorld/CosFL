c226c60bdda25701613151a3358267a6999444a5
IntHelloWorld <1009479460@qq.com>
2025-04-17 13:05:58 +0800

    Fix async error

--- Utils/path_manager.py
+++ Utils/path_manager.py
@@ -12,35 +12,32 @@ DEFAULT_VECTOR_STORE_NAME = "chroma"
 DEFAULT_PERSIST_FNAME = "docstore.json"
 
 log_config = {
-    'version': 1,
-    'formatters': {
-        'simple': {
-            'format': '%(levelname)s - %(asctime)s - %(message)s',
-            'datefmt': '%Y-%m-%d %H:%M:%S',
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s - %(asctime)s - %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
         }
     },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-            'level': 'DEBUG',
-            'stream': 'ext://sys.stdout',
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "level": "DEBUG",
+            "stream": "ext://sys.stdout",
         },
-        'file': {
-            'class': 'logging.FileHandler',
-            'formatter': 'simple',
-            'level': 'DEBUG',
-            'filename': '',
-            'mode': 'w',
-            'encoding': 'utf-8',
-        }
+        "file": {
+            "class": "logging.FileHandler",
+            "formatter": "simple",
+            "level": "DEBUG",
+            "filename": "",
+            "mode": "w",
+            "encoding": "utf-8",
+        },
     },
-    'loggers': {
-        'default': {
-            'handlers': ['file', 'console'],
-            'level': 'DEBUG'
-        },
-    }
+    "loggers": {
+        "default": {"handlers": ["file", "console"], "level": "DEBUG"},
+    },
 }
 
 
@@ -52,11 +49,11 @@ class Config:
             setattr(self, key, value)
 
 
-class PathManager():
+class PathManager:
 
     def __init__(self, args):
         self.root_path = Path(__file__).resolve().parents[1].as_posix()
-        
+
         self.verbose = args.verbose
 
         # bug info
@@ -75,17 +72,24 @@ class PathManager():
             self.config_name,
             f"{args.version}",
             args.project,
-            f"{args.project}-{args.bugID}")
-        self.retrieved_nodes_file = os.path.join(self.res_path, "retrieved_nodes.pkl")
+            f"{args.project}-{args.bugID}",
+        )
+        self.retrieved_nodes_file = os.path.join(
+            self.res_path, "retrieved_nodes.pkl"
+        )
         self.res_file = os.path.join(self.res_path, "result.json")
         self.projects_path = os.path.join(self.root_path, "Projects")
-        self.bug_path = os.path.join(self.projects_path, args.project, str(args.bugID))
-        self.test_failure_file = os.path.join(self.bug_path, "test_failure.pkl")
+        self.bug_path = os.path.join(
+            self.projects_path, args.project, str(args.bugID)
+        )
+        self.test_failure_file = os.path.join(
+            self.bug_path, "test_failure.pkl"
+        )
         self.method_nodes_file = os.path.join(self.bug_path, "nodes.pkl")
         self.proj_tmp_path = os.path.join(
             self.output_path,
             self.config_hash,
-            f"{args.version}-{args.project}-{args.bugID}"
+            f"{args.version}-{args.project}-{args.bugID}",
         )
         self.buggy_path = os.path.join(self.proj_tmp_path, "buggy")
         self.fixed_path = os.path.join(self.proj_tmp_path, "fixed")
@@ -102,9 +106,7 @@ class PathManager():
         self.src_class_prefix = None
         self.test_class_prefix = None
 
-        for path in [
-            self.res_path,
-            self.bug_path]:
+        for path in [self.res_path, self.bug_path]:
             if not os.path.exists(path):
                 os.makedirs(path)
 
@@ -119,9 +121,11 @@ class PathManager():
             "DocStores",
             f"{self.config.models.summary.cache_name}(module_size_{self.config.hyper.min_module_size}-{self.config.hyper.max_module_size})",
             args.project,
-            self.bug_name
+            self.bug_name,
         )
-        self.doc_store_file = os.path.join(self.doc_stores_dir, DEFAULT_PERSIST_FNAME)
+        self.doc_store_file = os.path.join(
+            self.doc_stores_dir, DEFAULT_PERSIST_FNAME
+        )
         if not os.path.exists(self.doc_stores_dir):
             os.makedirs(self.doc_stores_dir, exist_ok=True)
 
@@ -131,7 +135,7 @@ class PathManager():
             "VectorStores",
             f"{self.config.models.embed.cache_name}(module_size_{self.config.hyper.min_module_size}-{self.config.hyper.max_module_size})",
             args.project,
-            DEFAULT_VECTOR_STORE_NAME
+            DEFAULT_VECTOR_STORE_NAME,
         )
         if not os.path.exists(self.vector_stores_dir):
             os.makedirs(self.vector_stores_dir, exist_ok=True)
@@ -158,22 +162,28 @@ class PathManager():
                 "results",
                 self.project,
                 str(self.bug_id),
-                f"{self.sbfl_formula}.ranking.csv"
+                f"{self.sbfl_formula}.ranking.csv",
             )
 
         # init logger with time
-        log_config['handlers']['file']['filename'] = os.path.join(self.res_path, f"{int(time())}.log")
+        log_config["handlers"]["file"]["filename"] = os.path.join(
+            self.res_path, f"{int(time())}.log"
+        )
         logging.config.dictConfig(log_config)
         self.logger = logging.getLogger("default")
 
     def get_class_file(self, class_name):
-        class_file = os.path.join(self.buggy_path,
-                                  self.src_prefix,
-                                  class_name.split("$")[0].replace(".", "/") + ".java")
+        class_file = os.path.join(
+            self.buggy_path,
+            self.src_prefix,
+            class_name.split("$")[0].replace(".", "/") + ".java",
+        )
         if not os.path.exists(class_file):
-            class_file = os.path.join(self.buggy_path,
-                                      self.test_prefix,
-                                      class_name.split("$")[0].replace(".", "/") + ".java")
+            class_file = os.path.join(
+                self.buggy_path,
+                self.test_prefix,
+                class_name.split("$")[0].replace(".", "/") + ".java",
+            )
         if not os.path.exists(class_file):
             self.logger.warning(f"{class_file} not exists!")
             return None
@@ -181,5 +191,5 @@ class PathManager():
 
     def get_md5_hash(self, input_string):
         md5_hash = hashlib.md5()
-        md5_hash.update(input_string.encode('utf-8'))
+        md5_hash.update(input_string.encode("utf-8"))
         return md5_hash.hexdigest()
